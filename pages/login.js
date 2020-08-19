4f894f642a9489521ffadc6c7d3f6a96bee5d8f7
Blai Juárez <blai.juarez.contractor@bbva.com>
2020-08-19 18:33:45 +0200

    Se crea el login

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,86 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 import Layout from '../components/Layout'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import { useMutation, gql } from '@apollo/client'
+
+const AUTENTICAR_USUARIO = gql`
+  mutation autenticarUsuario($input: AutenticarInput) {
+    autenticarUsuario(input: $input) {
+      token
+    }
+  }
+`
 
 export default function Login() {
+  // State para el mensaje
+  const [mensaje, guardarMensaje] = useState(null)
+
+  // Crear nuevo usuario
+  const [ autenticarUsuario ] = useMutation(AUTENTICAR_USUARIO)
+
+  // Routing
+  const router = useRouter()
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+                .email('El email no es válido')
+                .required('El email es obligatorio'),
+      password: Yup.string()
+                .required('El password es obligatorio')
+                .min(6)
+    }),
+    onSubmit: async values => {
+      try {
+        const { data } = await autenticarUsuario({
+          variables: {
+            input: { ...values }
+          }
+        })
+
+        // Usuario creado correctamente
+        guardarMensaje(`Autenticando...`)
+        // Guardar token en sesion storage
+        const { autenticarUsuario: { token } } = data;
+        sessionStorage.setItem('token', token)
+
+        setTimeout(() => {
+          guardarMensaje(null)
+          router.push('/')
+        }, 2000)
+
+      } catch (error) {
+        guardarMensaje(error.message.replace('GraphQL error:', ''))
+        setTimeout(() => guardarMensaje(null), 3000)
+      }
+    }
+  })
+
+  const mostrarMensaje = () => {
+    return (
+      <div className="bg-white py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">
+        <p>{mensaje}</p>
+      </div>
+    )
+  }
+
   return (
     <>
       <Layout>
+        { mensaje && mostrarMensaje() }
         <h1 className="text-center text-2xl text-white font-light">Login</h1>
         <div className="flex justify-center mt-5">
           <div className="w-full max-w-sm">
-            <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+            <form 
+              className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+              onSubmit={formik.handleSubmit}
+            >
               <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                   Email
@@ -17,8 +90,18 @@ export default function Login() {
                   id="email"
                   type="email"
                   placeholder="Email usuario"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
                 />
               </div>
+
+              { formik.touched.email && formik.errors.email ? (
+                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                  <p className="font-bold">Error</p>
+                  <p>{ formik.errors.email }</p>
+                </div>
+              ) : null }
 
               <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
@@ -29,8 +112,18 @@ export default function Login() {
                   id="password"
                   type="password"
                   placeholder="Password usuario"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
                 />
               </div>
+
+              { formik.touched.password && formik.errors.password ? (
+                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                  <p className="font-bold">Error</p>
+                  <p>{ formik.errors.password }</p>
+                </div>
+              ) : null }
 
               <input
                 type="submit"
