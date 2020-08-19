d2399d2825ea749b1b627ba24d73c554e6d2d294
Blai Juárez <blai.juarez.contractor@bbva.com>
2020-08-19 18:32:37 +0200

    Se inyecta token a las cabecceras

--- config/apollo.js
+++ config/apollo.js
@@ -1,12 +1,26 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from 'apollo-link-context'
 import fetch from 'node-fetch'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+  fetch
+})
+const authLink = setContext((_, { headers }) => {
+  // Obtener token de autenticación
+  const token = sessionStorage.getItem('token')
+
+  return {
+    headers: {
+    ...headers,
+    authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
 
 const client = new ApolloClient({
   connectToDevTools: true,
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: '',
-    fetch
-  })
+  link: authLink.concat(httpLink)
 })
 export default client
