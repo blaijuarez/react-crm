8fac6121d6ac9008fc5026fe4d1b686f6e046964
Blai Juárez <blai.juarez.contractor@bbva.com>
2020-08-21 12:15:41 +0200

    Linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,28 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error'
+}
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true,
-        "node": true
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "plugin:react/recommended",
-        "standard"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-};
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': RULES.OFF,
+    'react/prop-types': RULES.OFF
+  }
+}
